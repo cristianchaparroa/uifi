a9421b3e728b8518bd11af9272ec8681f1213abc
Cristian Chaparro <cristianchaparroa@gmail.com>
2015-03-25 21:50:44 -0500

    Agregar usuario administrador

--- create.basic.users.sh
+++ create.basic.users.sh
@@ -5,3 +5,6 @@ sudo php app/console fos:user:promote cristian ROLE_DIRECTOR
 
 sudo php app/console  fos:user:create  admin cristianchaparroa@gmail.com 1032
 sudo php app/console fos:user:promote admin ROLE_ADMIN
+
+sudo php app/console fos:user:create paulogaona alonso.cursos@gmail.com 1234
+sudo php app/console fos:user:promote admin ROLE_ADMIN
