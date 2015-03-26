3c36af8a1da1e0b3f934531ee3b06947c3814572
Cristian Chaparro <cristianchaparroa@gmail.com>
2015-03-25 22:06:59 -0500

    Redireccion de / al login

---
+++ src/AppBundle/Controller/AppController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class AppController extends Controller
+{
+
+  /**
+   * Función que se encarga de redireccionar la raíz de la aplicación al login
+   *
+   * @Route( "/" , name="root")
+   *
+  */
+  public function indexAction()
+  {
+      return $this->redirect($this->generateUrl('login'));
+  }
+}
