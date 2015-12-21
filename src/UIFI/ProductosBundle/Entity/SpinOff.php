80af75ea14cf6c399c6375c42492f1221859d1cf
Cristian <cristianchaparroa@gmail.com>
2015-12-20 21:07:56 -0500

    Creacion de la entidad de Empresas de base tecnológica (Spin-off)

---
+++ src/UIFI/ProductosBundle/Entity/SpinOff.php
@@ -0,0 +1,338 @@
+<?php
+
+namespace UIFI\ProductosBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ *  Normas y regulaciones
+ *
+ * @ORM\Table(name="spinoff")
+ * @ORM\Entity(repositoryClass="UIFI\ProductosBundle\Repository\SpinoffRepository")
+ */
+class SpinOff {
+      /**
+       * @var integer
+       *
+       * @ORM\Column(name="id", type="integer")
+       * @ORM\Id
+       * @ORM\GeneratedValue(strategy="AUTO")
+       */
+      private $id;
+      /**
+       * @var string
+       *
+       * @ORM\Column(name="tipo", type="string",nullable=true)
+       */
+      private $tipo;
+      /**
+      * Titulo del artículo de investigación
+       * @var string
+       *
+       * @ORM\Column(name="titulo", type="string", length=10000,nullable=true)
+       */
+      private $titulo;
+      /**
+       * @var string
+       *
+       * @ORM\Column(name="pais", type="string", length=60, nullable=true )
+       */
+      private $pais;
+
+      /**
+       *
+       * @ORM\Column(name="mes", type="string",length=12,nullable=true)
+       */
+      private $mes;
+      /**
+       *
+       * @ORM\Column(name="anual", type="string",length=10,nullable=true)
+       */
+      private $anual;
+      /**
+       *
+       * @ORM\Column(name="nit", type="string",length=20,nullable=true)
+       */
+      private $nit;
+      /**
+       *
+       * @ORM\Column(name="fecha_registro_camara", type="string",length=20,nullable=true)
+       */
+      private $fechaRegistroCamara;
+
+      /**
+       * @var string
+       *
+       * @ORM\Column(name="nombre_grupo", type="string", length=255)
+       */
+      private $nombreGrupo;
+      /**
+       * Codigo del grupo en el cual fue publicado el articulo
+       *
+       * @ORM\Column(name="grupo", type="string", length=255)
+       */
+      private $grupo;
+      /**
+       * Integrantes de un grupo de un investigacion que publicaron el articulo.
+       *
+       * @ORM\ManyToMany(targetEntity="UIFI\IntegrantesBundle\Entity\Integrante", mappedBy="articulos")
+      */
+      private $integrantes;
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->integrantes = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set tipo
+     *
+     * @param string $tipo
+     * @return SpinOff
+     */
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+
+        return $this;
+    }
+
+    /**
+     * Get tipo
+     *
+     * @return string 
+     */
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+
+    /**
+     * Set titulo
+     *
+     * @param string $titulo
+     * @return SpinOff
+     */
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    /**
+     * Get titulo
+     *
+     * @return string 
+     */
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Set pais
+     *
+     * @param string $pais
+     * @return SpinOff
+     */
+    public function setPais($pais)
+    {
+        $this->pais = $pais;
+
+        return $this;
+    }
+
+    /**
+     * Get pais
+     *
+     * @return string 
+     */
+    public function getPais()
+    {
+        return $this->pais;
+    }
+
+    /**
+     * Set mes
+     *
+     * @param string $mes
+     * @return SpinOff
+     */
+    public function setMes($mes)
+    {
+        $this->mes = $mes;
+
+        return $this;
+    }
+
+    /**
+     * Get mes
+     *
+     * @return string 
+     */
+    public function getMes()
+    {
+        return $this->mes;
+    }
+
+    /**
+     * Set anual
+     *
+     * @param string $anual
+     * @return SpinOff
+     */
+    public function setAnual($anual)
+    {
+        $this->anual = $anual;
+
+        return $this;
+    }
+
+    /**
+     * Get anual
+     *
+     * @return string 
+     */
+    public function getAnual()
+    {
+        return $this->anual;
+    }
+
+    /**
+     * Set nit
+     *
+     * @param string $nit
+     * @return SpinOff
+     */
+    public function setNit($nit)
+    {
+        $this->nit = $nit;
+
+        return $this;
+    }
+
+    /**
+     * Get nit
+     *
+     * @return string 
+     */
+    public function getNit()
+    {
+        return $this->nit;
+    }
+
+    /**
+     * Set fechaRegistroCamara
+     *
+     * @param string $fechaRegistroCamara
+     * @return SpinOff
+     */
+    public function setFechaRegistroCamara($fechaRegistroCamara)
+    {
+        $this->fechaRegistroCamara = $fechaRegistroCamara;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaRegistroCamara
+     *
+     * @return string 
+     */
+    public function getFechaRegistroCamara()
+    {
+        return $this->fechaRegistroCamara;
+    }
+
+    /**
+     * Set nombreGrupo
+     *
+     * @param string $nombreGrupo
+     * @return SpinOff
+     */
+    public function setNombreGrupo($nombreGrupo)
+    {
+        $this->nombreGrupo = $nombreGrupo;
+
+        return $this;
+    }
+
+    /**
+     * Get nombreGrupo
+     *
+     * @return string 
+     */
+    public function getNombreGrupo()
+    {
+        return $this->nombreGrupo;
+    }
+
+    /**
+     * Set grupo
+     *
+     * @param string $grupo
+     * @return SpinOff
+     */
+    public function setGrupo($grupo)
+    {
+        $this->grupo = $grupo;
+
+        return $this;
+    }
+
+    /**
+     * Get grupo
+     *
+     * @return string 
+     */
+    public function getGrupo()
+    {
+        return $this->grupo;
+    }
+
+    /**
+     * Add integrantes
+     *
+     * @param \UIFI\IntegrantesBundle\Entity\Integrante $integrantes
+     * @return SpinOff
+     */
+    public function addIntegrante(\UIFI\IntegrantesBundle\Entity\Integrante $integrantes)
+    {
+        $this->integrantes[] = $integrantes;
+
+        return $this;
+    }
+
+    /**
+     * Remove integrantes
+     *
+     * @param \UIFI\IntegrantesBundle\Entity\Integrante $integrantes
+     */
+    public function removeIntegrante(\UIFI\IntegrantesBundle\Entity\Integrante $integrantes)
+    {
+        $this->integrantes->removeElement($integrantes);
+    }
+
+    /**
+     * Get integrantes
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getIntegrantes()
+    {
+        return $this->integrantes;
+    }
+}
