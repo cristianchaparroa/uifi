4361fbd8c2648dd46a381c11943631259c4ab0ca
Cristian <cristianchaparroa@gmail.com>
2015-12-26 10:56:41 -0500

    Elimando los controladores de prueba vacios

--- src/UIFI/ReportesBundle/Tests/Controller/CapitulosLibroReportesControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace UIFI\ReportesBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class CapitulosLibroReportesControllerTest extends WebTestCase
-{
-}
--- src/UIFI/ReportesBundle/Tests/Controller/ReporteControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace UIFI\ReportesBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class ReporteControllerTest extends WebTestCase
-{
-}
